d219a8f38a8a71a2571ce15c5d522e6df0dc03a9
Karina Pichardo <pichardokarina21@yahoo.com>
2020-03-13 20:32:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return lowerCase;
+}
+
+var lowerCase = 'I can\'t hear you!'
+  
+lowerCase.toLowerCase() === lowerCase 
+
+var upperCase = 'YES INDEED!'
+  
+upperCase.toUpperCase() === upperCase 
+  
+var mixedCase = 'I love you, too.'
+  
+mixedCase.toLowerCase() === mixedCase
+
+mixedCase.toUpperCase() === mixedCase
