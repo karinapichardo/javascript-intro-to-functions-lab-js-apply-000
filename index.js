0b6531daa02151ada9dc76541f9cb83d621d2220
Karina Pichardo <pichardokarina21@yahoo.com>
2020-03-13 22:16:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-   
+   if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+}
+else if (string === string.toUpperCase()) {
+    return "YES INDEED!";
+} else if ('I love you, Grandma.') {
+  return 'I love you, too.'
+}
 }
 var lowerCase = 'I can\'t hear you!'
   
